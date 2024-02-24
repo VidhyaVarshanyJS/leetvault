ea9fab8ee865a58a5f6b3dea228aeca40d1e5316
Vidhya Varshany <76642252+VidhyaVarshanyJS@users.noreply.github.com>
2024-02-24 21:52:01 +0530

    Time: 19 ms (49.51%), Space: 11.9 MB (63.96%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        # add open ; open < n
+        # add close close < open 
+        # base case open = close = n
+        stack = []
+        res = []
+        def backtrack(openN, closeN):
+            if openN == closeN == n:
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closeN)
+                stack.pop()
+            if closeN < openN:
+                stack.append(")")
+                backtrack(openN ,  closeN + 1)
+                stack.pop()
+        backtrack(0, 0)
+        return res
