3dd87375d8799eb1b28ca7edaa220de8fccdbd3c
Vidhya Varshany <76642252+VidhyaVarshanyJS@users.noreply.github.com>
2024-05-17 18:53:56 +0530

    Time: 732 ms (48.22%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -2,6 +2,7 @@
 select 
 query_name,
 ifnull(round(sum(rating/ position)/count(*),2),0) as quality ,
-round(avg(if(rating<3,1,0)) *100,2) as poor_query_percentage
+ifnull(round(avg(if(rating<3,1,0)) *100,2),0) as poor_query_percentage
 from queries
+where query_name is not null
 group by query_name
