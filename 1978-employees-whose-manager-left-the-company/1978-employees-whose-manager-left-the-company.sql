4d588cbc6571b448e2caf67efe728aa494c2bdf8
Vidhya Varshany <76642252+VidhyaVarshanyJS@users.noreply.github.com>
2024-05-31 16:12:57 +0530

    Time: 524 ms (96.61%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,11 +1,12 @@
-with cte as 
-(select 
-e.*
-from employees e 
-join employees m
-on e.employee_id = m.employee_id 
-where e.salary < 30000 )
-
-select employee_id 
-from cte where manager_id not in (select distinct employee_id from employees)
-order by employee_id
+select 
+    employee_id
+from 
+    Employees
+where 
+    salary < 30000
+    and 
+    manager_id not in (
+                        select employee_id from Employees
+                      )
+order by 
+    employee_id;
